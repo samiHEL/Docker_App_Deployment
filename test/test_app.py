7b096a22e0a7afbf981b82d269492f367ba09774
samiHEL <61313022+samiHEL@users.noreply.github.com>
2024-02-02 13:38:17 +0100

    Update test_app.py

--- test/test_app.py
+++ test/test_app.py
@@ -1,18 +1,14 @@
 import pytest
-from flask import Flask
-from app import app
+import requests
+#from frontend.streamlit_app import API_ENDPOINT
+API_ENDPOINT = 'http://localhost:5000/api/get_data'
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
-
-def test_login(client):
-    # Test avec des fausses données
-    response = client.post('/login', data={'username': 'fake_user', 'password': 'fake_password'})
-    assert b'Invalid Credentials. Please try again.' in response.data
-
-    # Test avec de vraies données
-    response = client.post('/login', data={'username': 'admin', 'password': 'admin'})
-    assert b'Logged in as: admin' in response.data
+def test_api_response_not_empty():
+    # Faire l'appel à l'API
+    response = requests.get(API_ENDPOINT)
+    
+    # Vérifier que la réponse a un code HTTP 200 (OK)
+    assert response.status_code == 200
+    
+    # Vérifier que le JSON de la réponse n'est pas vide
+    #assert response.json() != []
